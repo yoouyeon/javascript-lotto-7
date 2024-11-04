cb8a076d9e5c2632d1aca842d5d38f8b9a659176
yoouyeon <jyeon.yoon59@gmail.com>
2024-11-04 21:49:23 +0900

    docs(output): JSDoc 주석 추가
    - 타입 정의 추가
    - description 보완

--- src/ui/LottoOutput.js
+++ src/ui/LottoOutput.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 import Output from './Output.js';
 
 /** @typedef {import ('../domains/Lotto.js').default} Lotto */
@@ -7,17 +9,30 @@ class LottoOutput extends Output {
 
   static #LOTTO_AMOUNT_MESSAGE = (amount) => `로또 ${amount}개를 구매했습니다.`;
 
+  /**
+   * @param {Array<number>} numbers - 로또 번호 배열
+   * @returns {string}
+   */
   static #LOTTO_NUMBERS_FORMAT = (numbers) =>
     `[${numbers.sort((a, b) => a - b).join(', ')}]`;
 
+  /**
+   * @param {string} resultDescription - 당첨 결과 설명
+   * @param {number} winningMoney - 결과별 당첨금
+   * @param {number} count - 당첨 갯수
+   * @returns {string}
+   */
   static #STATISTICS_FORMAT = (resultDescription, winningMoney, count) =>
     `${resultDescription} (${winningMoney.toLocaleString()}원) - ${count}개`;
 
+  /**
+   * @param {number} yieldRate - 수익률
+   * @returns {string}
+   */
   static #YIELD_FORMAT = (yieldRate) =>
     `총 수익률은 ${yieldRate.toFixed(1)}%입니다.`;
 
   /**
-   *
    * @param {Array<Lotto>} lottos - 구매한 로또 배열
    */
   static printPurchasedLotto(lottos) {
@@ -27,12 +42,19 @@ class LottoOutput extends Output {
     );
   }
 
+  /**
+   * @param {Array<import ('../types.js').LottoStatisticsType>} statistics
+   * @param {number} yieldRate
+   */
   static printResult(statistics, yieldRate) {
     this.#printStatistics(statistics);
     super.printNewLine();
     super.print(this.#YIELD_FORMAT(yieldRate));
   }
 
+  /**
+   * @param {Array<import ('../types.js').LottoStatisticsType>} statistics
+   */
   static #printStatistics(statistics) {
     super.print(this.#STATISTICS_HEADER_MESSAGE);
     statistics.forEach(({ description, winningMoney, count }) =>
