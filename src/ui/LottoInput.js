0289fc008313c902074c814e046ba217a346bf6d
yoouyeon <jyeon.yoon59@gmail.com>
2024-11-04 14:59:16 +0900

    refactor(input): 메소드 이름 변경
    "구매" 동작을 의미하는 단어를 purchase로 통일함

--- src/ui/LottoInput.js
+++ src/ui/LottoInput.js
@@ -1,14 +1,14 @@
 import Input from './Input.js';
 
 class LottoInput extends Input {
-  static #BUY_MESSAGE = '로또 구입 금액을 입력해주세요. (1장: 1,000원)\n';
+  static #PURCHASE_MESSAGE = '로또 구입 금액을 입력해주세요. (1장: 1,000원)\n';
 
   static #WINNING_NUMBERS_MESSAGE = '당첨 번호를 입력해 주세요.\n';
 
   static #BONUS_NUMBER_MESSAGE = '보너스 번호를 입력해 주세요.\n';
 
-  static async readBuyAmount() {
-    return super.readLine(this.#BUY_MESSAGE);
+  static async readTotalPurchaseCost() {
+    return super.readLine(this.#PURCHASE_MESSAGE);
   }
 
   static async readWinningNumbers() {
