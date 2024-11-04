2f0ef76078e4cf63983e89e5f60b1b445c721cf5
yoouyeon <jyeon.yoon59@gmail.com>
2024-11-04 21:49:01 +0900

    docs(input): JSDoc 주석 추가
    - 타입 정의 추가
    - description 보완

--- src/ui/Input.js
+++ src/ui/Input.js
@@ -1,7 +1,14 @@
+// @ts-check
+
 import { Console } from '@woowacourse/mission-utils';
 import inputValidation from '../validations/inputValidation.js';
 
 class Input {
+  /**
+   * @param {string} message - 입력 메시지
+   * @returns {Promise<string>}
+   * @description 입력을 받아 기본 유효성 검사를 수행한 후 반환한다.
+   */
   static async readLine(message) {
     const input = await Console.readLineAsync(message);
     inputValidation.validate(input);
--- src/ui/LottoInput.js
+++ src/ui/LottoInput.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 import bonusNumberValidation from '../validations/bonusNumberValidation.js';
 import purchaseValidation from '../validations/purchaseValidation.js';
 import winningNumbersValidation from '../validations/winningNumbersValidation.js';
@@ -10,12 +12,20 @@ class LottoInput extends Input {
 
   static #BONUS_NUMBER_MESSAGE = '보너스 번호를 입력해 주세요.\n';
 
+  /**
+   * @returns {Promise<number>}
+   * @description 구매 금액을 입력받아 유효성 검사를 수행한 후 반환한다.
+   */
   static async readTotalPurchaseCost() {
     const purchaseCost = Number(await super.readLine(this.#PURCHASE_MESSAGE));
     purchaseValidation.validate(purchaseCost);
     return purchaseCost;
   }
 
+  /**
+   * @returns {Promise<Array<number>>}
+   * @description 당첨 번호를 입력받아 유효성 검사를 수행한 후 반환한다.
+   */
   static async readWinningNumbers() {
     const winningNumbers = (await super.readLine(this.#WINNING_NUMBERS_MESSAGE))
       .split(',')
@@ -24,6 +34,11 @@ class LottoInput extends Input {
     return winningNumbers;
   }
 
+  /**
+   * @param {Array<number>} winningNumbers - 당첨 번호
+   * @returns {Promise<number>}
+   * @description 보너스 번호를 입력받아 유효성 검사를 수행한 후 반환한다.
+   */
   static async readBonusNumber(winningNumbers) {
     const bonusNumber = Number(
       await super.readLine(this.#BONUS_NUMBER_MESSAGE)
