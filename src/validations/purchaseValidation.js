064ddc016aadd3ef3b5b4d6284516f7ade0f1170
yoouyeon <jyeon.yoon59@gmail.com>
2024-11-04 23:55:14 +0900

    fix(validation): purchaseValidation 오류 수정
    숫자로 변환해서 유효성 검사하도록 변경

--- src/validations/purchaseValidation.js
+++ src/validations/purchaseValidation.js
@@ -9,15 +9,15 @@ import SINGLE_LOTTO_PRICE from '../constants/SingleLottoPrice.js';
 const purchaseValidation = Object.freeze({
   RULE_SET: Object.freeze({
     notNumber: Object.freeze({
-      isValid: (priceInput) => !Number.isNaN(priceInput),
+      isValid: (priceInput) => !Number.isNaN(Number(priceInput)),
       errorMessage: '구매 금액은 숫자로 입력해주세요.',
     }),
     notEnough: Object.freeze({
-      isValid: (priceInput) => priceInput >= SINGLE_LOTTO_PRICE,
+      isValid: (priceInput) => Number(priceInput) >= SINGLE_LOTTO_PRICE,
       errorMessage: `${SINGLE_LOTTO_PRICE}원 이상의 금액을 입력해주세요.`,
     }),
     notDivisible: Object.freeze({
-      isValid: (priceInput) => priceInput % SINGLE_LOTTO_PRICE === 0,
+      isValid: (priceInput) => Number(priceInput) % SINGLE_LOTTO_PRICE === 0,
       errorMessage: `${SINGLE_LOTTO_PRICE}원 단위로 입력해주세요.`,
     }),
   }),
