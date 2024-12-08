94b8eb2bf59c97803ec7409caa8b73d13ea9b448
yoouyeon <jyeon.yoon59@gmail.com>
2024-12-08 19:24:51 +0900

    feat(lotto): 로또 validate 기능 구체화
    - 로또 번호 조건 구체화
    - 에러 테스트케이스 추가
    - 기능 목록 업데이트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,6 +1,13 @@
 import Lotto from '../src/Lotto';
 
 describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개 이하이면 예외가 발생한다.', () => {
+    expect(() => {
+      // eslint-disable-next-line no-new
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow('[ERROR]');
+  });
+
   test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       // eslint-disable-next-line no-new
@@ -8,7 +15,6 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
   test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       // eslint-disable-next-line no-new
@@ -16,5 +22,34 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test.each([
+    { input: [0, 2, 3, 4, 5, 6], case: '1보다 작은 숫자' },
+    { input: [1, 2, 3, 4, 5, 46], case: '45보다 큰 숫자' },
+  ])('로또 번호에 $case가 있으면 예외가 발생한다.', ({ input }) => {
+    expect(() => {
+      // eslint-disable-next-line no-new
+      new Lotto(input);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호가 정수가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      // eslint-disable-next-line no-new
+      new Lotto([1, 2, 3, 4, 5, 6.5]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호가 숫자가 아니면 예외가 발생한다.', () => {
+    expect(() => {
+      // eslint-disable-next-line no-new
+      new Lotto([1, 2, 3, 4, 5, '6a']);
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호가 모든 조건을 만족하면 에러가 발생하지 않는다.', () => {
+    expect(() => {
+      // eslint-disable-next-line no-new
+      new Lotto([1, 2, 3, 4, 5, 6]);
+    }).not.toThrow();
+  });
 });
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,32 @@
+// @ts-check
+
+import CustomError from './CustomError.js';
+
+const LOTTO_LENGTH = 6;
 class Lotto {
   #numbers;
 
+  /**
+   * @param {(number)[]} numbers
+   */
   constructor(numbers) {
     Lotto.#validate(numbers);
     this.#numbers = numbers;
   }
 
+  /**
+   * @param {(number)[]} numbers
+   */
   static #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
-    }
+    if (numbers.length !== LOTTO_LENGTH) throw new CustomError('로또 번호는 6개여야 합니다.');
+    if (new Set(numbers).size !== LOTTO_LENGTH)
+      throw new CustomError('로또 번호는 중복되지 않아야 합니다.');
+    numbers.forEach((number) => {
+      if (number < 1 || number > 45) throw new CustomError('로또 번호는 1과 45 사이여야 합니다.');
+      if (!Number.isInteger(number)) throw new CustomError('로또 번호는 정수여야 합니다.');
+      if (Number.isNaN(number)) throw new CustomError('로또 번호는 숫자여야 합니다.');
+    });
   }
-
-  // TODO: 추가 기능 구현
 }
 
 export default Lotto;
